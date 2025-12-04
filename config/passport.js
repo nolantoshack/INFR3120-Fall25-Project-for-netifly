1015dd673d9e02c412728e4124786605ada23237
argiriskappos <argiris.kappos@ontariotechu.net>
2025-12-04 10:58:41 -0500

    Create passport.js

---
+++ config/passport.js
@@ -0,0 +1,45 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("../models/User");
+
+module.exports = function (passport) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback"
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          const email = profile.emails[0].value;
+
+          let user = await User.findOne({ email });
+
+          if (!user) {
+            user = await User.create({
+              fullName: profile.displayName,
+              email,
+              password: null,
+              role: "Driver",
+              googleId: profile.id
+            });
+          }
+
+          return done(null, user);
+        } catch (error) {
+          console.error(error);
+          done(error, null);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    done(null, user);
+  });
+};
